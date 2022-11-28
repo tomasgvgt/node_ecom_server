784d45a5e43697a31526e02c8d16796e6db861dd
tomasgvgt <tomasgvg.t@gmail.com>
2022-11-28 15:02:00 -0500

    Experimenting with api key in query params

--- middlewears/auth.js
+++ middlewears/auth.js
@@ -1,7 +1,7 @@
 const boom = require('@hapi/boom');
 
 function autentication(req, res, next){
-  if(req.headers['api'] === '321'){
+  if(req.query['api'] === '321'){
     next()
   }else{
     next(boom.unauthorized('Not authorized'))
